88a8af0a08e9d5238138f56c2f368f745508fd73
Manikandan Prabhu <manikandanrprabhu@gmail.com>
2021-12-14 10:24:25 +0530

    Fifth Commit

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+//For Preloader
 window.addEventListener('load', () =>{
     const preload = document.querySelector('.preload');
     preload.classList.add('preload-finish');
 }  
 )
+// To Hide Navbar while scroll ( Not Working Dont know Why...)
+var prevscrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentposition = window.pageYOffset;
+    if(prevscrollpos>currentposition){
+        document.getElementById("navtop").style.top ="0";
+    }
+    else{
+        document.getElementById("navtop").style.top ="-";
+    }
+    prevscrollpos = currentposition;
+}
