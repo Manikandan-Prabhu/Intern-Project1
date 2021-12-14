d2ff7ff4d18338ddb27579e6c1ec91919172519c
Manikandan Prabhu <manikandanrprabhu@gmail.com>
2021-12-14 14:47:36 +0530

    Tested some features but nothing works :(

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ window.addEventListener('load', () =>{
     preload.classList.add('preload-finish');
 }  
 )
-// To Hide Navbar while scroll ( Not Working Dont know Why...)
-var prevscrollpos = window.pageYOffset;
+// To Hide Navbar while scroll ( Not Working)
+/*var prevscrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentposition = window.pageYOffset;
     if(prevscrollpos>currentposition){
@@ -16,3 +16,16 @@ window.onscroll = function() {
     }
     prevscrollpos = currentposition;
 }
+
+// Background color Transition
+let page3 = document.querySelector('.page3');
+let height = page3.offsetHeight;
+window.addEventListener('scroll',bgcolor);
+function bgcolor(){
+    if(this.scrollY > height / 1.5){
+        document.body.classList.add(bg-color);
+    }else
+    {
+        document.body.classList.remove(bg-color);
+    }
+}*/
